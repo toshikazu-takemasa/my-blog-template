18069b81d1351026d5da173891799bf7d8db80c6
kabotya1985 <takema1989@gmail.com>
2022-12-01 19:41:56 +0900

    120103

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const prefixPath = !isProd ? '/mypage/org-contents/next-app' : '';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '/mypage/org-contents/next-app',
-  basePath: '/mypage/org-contents/next-app',
+  // assetPrefix: '/mypage/org-contents/next-app',
+  // basePath: '/mypage/org-contents/next-app',
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
