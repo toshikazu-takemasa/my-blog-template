47f9a84fca33934b14835a80e5392b65099e00cd
kabotya1985 <takema1989@gmail.com>
2022-12-01 19:31:35 +0900

    1200102

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production';
+const prefixPath = !isProd ? '/mypage/org-contents/next-app' : '';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/mypage/org-contents/next-app',
+  basePath: '/mypage/org-contents/next-app',
   reactStrictMode: true,
+  trailingSlash: true,
   swcMinify: true,
-}
+  images: {
+    //loader: 'custom',
+    domains: ['images.microcms-assets.io'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  options: {
+    // https://purgecss.com/safelisting.html#patterns
+    safelist: {
+      standard: [/^bg-/, /^text-/],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
