a4a3bec277441c9982b53e386bef7842ba3f2661
kabotya1985 <takema1989@gmail.com>
2022-12-02 17:12:50 +0900

    120203

--- src/libs/client.js
+++ src/libs/client.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN || 'zx5efkkc2e',
-  apiKey: process.env.API_KEY || 'd4d164926c3046d182ffb6d05b8de6f233e9',
+  serviceDomain: 'zx5efkkc2e',
+  apiKey: 'd4d164926c3046d182ffb6d05b8de6f233e9',
 });
