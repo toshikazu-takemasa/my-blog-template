b761859585f4f75a971375ba86534733f922ba5f
tosi1985 <tosi1985@gmail.com>
2023-09-10 21:11:34 +0900

    23091002

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,17 +12,11 @@ export default function Home({ article }) {
       <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
         {/* ピックアップ */}
         <article className="py-6">
-          <p className="sm:w-1/2 py-3 sm:mx-auto text-xl font-bold">
-            ピックアップ
-          </p>
+          <p className="sm:w-1/2 py-3 sm:mx-auto text-xl font-bold">ピックアップ</p>
           <img className="sm:w-1/2 mx-auto" src={"/img/minoyaki.jpg"} />
           <div className="flex items-center sm:w-1/2 sm:mx-auto">
-            <p className=" my-2 p-1 bg-green-300 text-white text-base">
-              コラム
-            </p>
-            <p className="text-lg font-bold">
-              おしゃれな美濃焼コップで淹れるコーヒー
-            </p>
+            <p className=" my-2 p-1 bg-green-300 text-white text-base">コラム</p>
+            <p className="text-lg font-bold">おしゃれな美濃焼コップで淹れるコーヒー</p>
           </div>
         </article>
         {/* ピックアップ */}
@@ -41,29 +35,13 @@ export default function Home({ article }) {
         <div className="mx-auto sm:w-1/2 grid sm:grid-cols-2 lg:grid-cols-3  gap-4 md:gap-6">
           {/* <!-- article - start --> */}
           {article.map((article) => (
-            <Link
-              key={article.id}
-              href={`/recipe/${article.id}`}
-              legacyBehavior
-            >
+            <Link key={article.id} href={`/recipe/${article.id}`} legacyBehavior>
               <div className="flex flex-col">
                 <div className="group h-48 md:h-64 block bg-gray-100 overflow-hidden relative rounded-md">
-                  <img
-                    src={article.eyecatch.url}
-                    loader={microCMSLoader}
-                    loading="lazy"
-                    alt=""
-                    layout="fill"
-                    width={500}
-                    height={500}
-                    objectfit="contain"
-                    className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-                  />
+                  <img src={article.eyecatch.url} loader={microCMSLoader} loading="lazy" alt="" layout="fill" width={500} height={500} objectfit="contain" className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
                 </div>
                 <div className="flex items-center">
-                  <p className="my-2 p-1 bg-green-300 text-white text-base">
-                    {article.category.name}
-                  </p>
+                  <p className="my-2 p-1 bg-green-300 text-white text-base">{article.category.name}</p>
                   <p className="text-lg font-bold">{article.title}</p>
                 </div>
               </div>
@@ -78,7 +56,7 @@ export default function Home({ article }) {
 
 // データをテンプレートに受け渡す部分の処理を記述しま
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "article" });
+  const data = await client.get({ endpoint: "blogs" });
 
   return {
     props: {
--- src/pages/recipe/[id].js
+++ src/pages/recipe/[id].js
@@ -9,25 +9,12 @@ const microCMSLoader = ({ src, width, quality }) => {
 export default function RecipeId({ article }) {
   return (
     <main className="p-6 font-sans">
-      <img
-        className="sm:w-1/2 mx-auto"
-        src={article.eyecatch.url}
-        height={article.eyecatch.height}
-        width={article.eyecatch.width}
-        loader={microCMSLoader}
-        loading="lazy"
-        alt=""
-        layout="fill"
-        objectfit="contain"
-      />
+      <img className="sm:w-1/2 mx-auto" src={article.eyecatch.url} height={article.eyecatch.height} width={article.eyecatch.width} loader={microCMSLoader} loading="lazy" alt="" layout="fill" objectfit="contain" />
       <article className="mx-auto sm:w-1/2">
         <p className="mt-5 font-bold text-lg">ハミングウォーター公式レシピ</p>
         <h1 className="text-2xl mx-auto font-bold">{article.title}</h1>
         <p className="mt-5 text-sm">{article.recipe.maintext}</p>
-        <div
-          className="mt-5 rounded-md text-center"
-          style={{ backgroundColor: "#e7e3e0" }}
-        >
+        <div className="mt-5 rounded-md text-center" style={{ backgroundColor: "#e7e3e0" }}>
           <div
             className="p-4"
             dangerouslySetInnerHTML={{
@@ -60,24 +47,14 @@ export default function RecipeId({ article }) {
           }}
         />
       </article>
-      <img
-        className=" sm:w-1/2 mx-auto mt-3"
-        src={article.recipe.subimage.url}
-        height={article.recipe.subimage.height}
-        width={article.recipe.subimage.width}
-        loader={microCMSLoader}
-        loading="lazy"
-        alt=""
-        layout="fill"
-        objectfit="contain"
-      />
+      <img className=" sm:w-1/2 mx-auto mt-3" src={article.recipe.subimage.url} height={article.recipe.subimage.height} width={article.recipe.subimage.width} loader={microCMSLoader} loading="lazy" alt="" layout="fill" objectfit="contain" />
     </main>
   );
 }
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
-  const data = await client.get({ endpoint: "article" });
+  const data = await client.get({ endpoint: "blogs" });
 
   const paths = data.contents.map((content) => `/recipe/${content.id}`);
   return { paths, fallback: "blocking" };
@@ -86,7 +63,7 @@ export const getStaticPaths = async () => {
 // データをテンプレートに受け渡す部分の処理を記述
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const data = await client.get({ endpoint: "article", contentId: id });
+  const data = await client.get({ endpoint: "blogs", contentId: id });
 
   return {
     props: {
