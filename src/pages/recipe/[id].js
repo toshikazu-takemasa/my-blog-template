ffe399ae09a75caa992ce4514481189fdf423dee
kabotya1985 <takema1989@gmail.com>
2023-10-04 16:34:48 +0900

    231004

--- src/pages/recipe/[id].js
+++ src/pages/recipe/[id].js
@@ -7,47 +7,52 @@ const microCMSLoader = ({ src, width, quality }) => {
 };
 
 export default function RecipeId({ article }) {
+  const mainContents = article.content;
   return (
     <main className="p-6 font-sans">
       <img className="sm:w-1/2 mx-auto" src={article.eyecatch.url} height={article.eyecatch.height} width={article.eyecatch.width} loader={microCMSLoader} loading="lazy" alt="" layout="fill" objectfit="contain" />
       <article className="mx-auto sm:w-1/2">
         <p className="mt-5 font-bold text-lg">〇〇さんのレシピ</p>
         <h1 className="text-2xl mx-auto font-bold">{article.title}</h1>
-        <p className="mt-5 text-sm">{article.recipe.maintext}</p>
-        <div className="mt-5 rounded-md text-center" style={{ backgroundColor: "#e7e3e0" }}>
-          <div
-            className="p-4"
-            dangerouslySetInnerHTML={{
-              __html: `${article.recipe.zairyou}`,
-            }}
-          />
-        </div>
-        <h2 className="my-1 font-bold text-lg">食材</h2>
-        <div
-          style={{ backgroundColor: "#e7e3e0" }}
-          className="p-4 rounded-md"
-          dangerouslySetInnerHTML={{
-            __html: `${article.recipe.syokuzai}`,
-          }}
-        />
-        <h2 className="my-1 font-bold text-lg">調味料</h2>
-        <div
-          style={{ backgroundColor: "#e7e3e0" }}
-          className="p-4 rounded-md"
-          dangerouslySetInnerHTML={{
-            __html: `${article.recipe.spice}`,
-          }}
-        />
-        <h2 className="my-1 font-bold text-lg">手順</h2>
-        <div
-          className="p-4 rounded-md"
-          style={{ backgroundColor: "#e7e3e0" }}
-          dangerouslySetInnerHTML={{
-            __html: `${article.recipe.procedure}`,
-          }}
-        />
+        {mainContents.map((content) => (
+          <>
+            <p className="mt-5 text-sm">{content.maintext}</p>
+            <div className="mt-5 rounded-md text-left" style={{ backgroundColor: "#e7e3e0" }}>
+              <div
+                className="p-4"
+                dangerouslySetInnerHTML={{
+                  __html: `${content.zairyou}`,
+                }}
+              />
+            </div>
+            <h2 className="my-1 font-bold text-lg">食材</h2>
+            <div
+              style={{ backgroundColor: "#e7e3e0" }}
+              className="p-4 rounded-md"
+              dangerouslySetInnerHTML={{
+                __html: `${content.syokuzai}`,
+              }}
+            />
+            <h2 className="my-1 font-bold text-lg">調味料</h2>
+            <div
+              style={{ backgroundColor: "#e7e3e0" }}
+              className="p-4 rounded-md"
+              dangerouslySetInnerHTML={{
+                __html: `${content.spice}`,
+              }}
+            />
+            <h2 className="my-1 font-bold text-lg">手順</h2>
+            <div
+              className="p-4 rounded-md"
+              style={{ backgroundColor: "#e7e3e0" }}
+              dangerouslySetInnerHTML={{
+                __html: `${content.procedure}`,
+              }}
+            />
+            {content.subimage ? <img className=" sm:w-1/2 mx-auto mt-3" src={content.subimage.url} height={content.subimage.height} width={content.subimage.width} loader={microCMSLoader} loading="lazy" alt="" layout="fill" objectfit="contain" /> : ""}
+          </>
+        ))}
       </article>
-      <img className=" sm:w-1/2 mx-auto mt-3" src={article.recipe.subimage.url} height={article.recipe.subimage.height} width={article.recipe.subimage.width} loader={microCMSLoader} loading="lazy" alt="" layout="fill" objectfit="contain" />
     </main>
   );
 }
